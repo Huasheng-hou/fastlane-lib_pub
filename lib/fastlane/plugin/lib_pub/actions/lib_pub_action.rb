595c9873e699fb2c94c7b9f98d02ab27d5e48d80
Huasheng Hou <331292108@qq.com>
2020-01-11 14:20:17 +0800

    fix

--- lib/fastlane/plugin/lib_pub/actions/lib_pub_action.rb
+++ lib/fastlane/plugin/lib_pub/actions/lib_pub_action.rb
@@ -5,7 +5,28 @@ module Fastlane
   module Actions
     class LibPubAction < Action
       def self.run(params)
-        UI.message("The lib_pub plugin is working!")
+
+        UI.message "Parameter Version: #{params[:version]}"
+
+        version = params[:version]
+        spec_path = params[:spec_path];
+
+        if other_action.git_tag_exists(tag: version)
+          UI.message "tag #{version} exists!, Please remove it or use another version number."
+        else
+          other_action.add_git_tag(tag: version)
+          other_action.push_git_tags(tag: version)
+
+          other_action.version_bump_podspec(path: spec_path, version_number: version)
+
+          other_action.pod_push(path: spec_path, 
+            repo: params[:repo],
+            sources: params[:sources])
+
+          other_action.git_add(path: ".")
+          other_action.git_commit(path: ["."], message: "Update podspec")
+          other_action.push_to_git_remote
+        end
       end
 
       def self.description
@@ -27,11 +48,26 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "LIB_PUB_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :version,
+                                  env_name: "LIB_PUB_YOUR_OPTION",
+                               description: "The pod version",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :spec_path,
+                                  env_name: "LIB_PUB_YOUR_OPTION",
+                               description: "The pod spec path",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :repo,
+                                  env_name: "LIB_PUB_YOUR_OPTION",
+                               description: "The pod repo to publish",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :sources,
+                                  env_name: "LIB_PUB_YOUR_OPTION",
+                               description: "The pod sources",
+                                  optional: true,
+                                      type: Array)
         ]
       end
 
